e9bfb0b8c8c402e5d0c02af1e499971119113c4c
Raazarya12 <125205649+Raazarya12@users.noreply.github.com>
2024-05-03 20:21:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -46,7 +46,7 @@ NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'True')), False)
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://bossjk:jkboss@cluster0.uywmvse.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 #refer time, or user count
