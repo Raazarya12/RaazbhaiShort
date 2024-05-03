96abcc1127d6c8d2fed12e077fd8fe0f8c107a0f
Raazarya12 <125205649+Raazarya12@users.noreply.github.com>
2024-05-03 17:04:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -45,8 +45,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'True')), False)
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://RAAZARYA:RAAZARYA@cluster0.rllcggl.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "AAAA")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://bossjk:jkboss@cluster0.uywmvse.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 #refer time, or user count
